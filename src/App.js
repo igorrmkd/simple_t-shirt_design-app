acf6460ef685f9fa75c24f0d9ee01a8db80809d5
Igor Risteski <pirej30@yahoo.com>
2020-12-03 15:06:25 +0100

    initial Color changing and some cleanup

--- src/App.js
+++ src/App.js
@@ -1,43 +1,51 @@
 import "./App.css";
 import theShirt from "./img/background_tshirt.png";
 // import batman from "./img/batman2.png";
-import React from "react";
+import React, { useState } from "react";
+import ColorPicker from "./components/ColorPicker";
 import { FabricJSCanvas, useFabricJSEditor } from "fabricjs-react";
 
 export default function App() {
   const { editor, onReady } = useFabricJSEditor();
-  // const onAddCircle = () => {
-  //   editor.addCircle();
-  // };
-  // const onAddRectangle = () => {
-  //   editor.addRectangle();
-  // };
+  const onAddCircle = () => {
+    editor.addCircle();
+  };
+  const onAddRectangle = () => {
+    editor.addRectangle();
+  };
+
+  const [shirtColor, setShirtColor] = useState("");
+
+  console.log(shirtColor);
+
+  function changeColor(event) {
+    event.preventDefault();
+    setShirtColor("green");
+  }
+  console.log(shirtColor);
 
   return (
     <div className="App">
       <h1>Simple t-shirt designer with FabricJS and ReactJS</h1>
-      {/* <button onClick={onAddCircle}>Add circle</button>
-      <button onClick={onAddRectangle}>Add Rectangle</button> */}
+      <button onClick={onAddCircle}>Add circle</button>
+      <button onClick={onAddRectangle}>Add Rectangle</button>
       <FabricJSCanvas className="sample-canvas" onReady={onReady} />
-      <img src={theShirt} alt="theShirt" className="shirt" />
+      <img
+        style={{ backgroundColor: shirtColor }}
+        src={theShirt}
+        alt="theShirt"
+        className="shirt"
+      />
 
-      {/* image upload */}
+      <button onClick={changeColor}>Color</button>
 
-      {/* <label htmlFor="tshirt-design">T-Shirt Design:</label>
-      <select id="tshirt-design">
-        <option value="">Select one of our designs ...</option>
-        <option value={batman}>Batman</option>
-      </select> */}
-
-      {/* change the color of the T-Shirt */}
-      <label htmlFor="tshirt-color">T-Shirt Color:</label>
-      <select id="tshirt-color">
-        <option value="#fff">White</option>
-        <option value="#000">Black</option>
-        <option value="#f00">Red</option>
-        <option value="#008000">Green</option>
-        <option value="#ff0">Yellow</option>
-      </select>
+      <ColorPicker />
     </div>
   );
 }
+
+// value="#fff">White
+// value="#000">Black
+// value="#f00">Red
+// value="#008000">Green
+// value="#ff0">Yellow
---
+++ src/components/ColorPicker.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function ColorPicker(props) {
+  return (
+    <div>
+      <p> from ColorPicker</p>
+      {/* {console.log(props)} */}
+    </div>
+  );
+}
+
+export default ColorPicker;
