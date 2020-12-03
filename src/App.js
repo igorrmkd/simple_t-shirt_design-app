1cdb974fc1be6db2970d23d0da81aa33416ecf52
Igor Risteski <pirej30@yahoo.com>
2020-12-03 01:43:05 +0100

    Install fabricjs-react, make it work with React, and cleanup a bit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import "./App.css";
 
-function App() {
+import React from "react";
+import { FabricJSCanvas, useFabricJSEditor } from "fabricjs-react";
+
+export default function App() {
+  const { editor, onReady } = useFabricJSEditor();
+  const onAddCircle = () => {
+    editor.addCircle();
+  };
+  const onAddRectangle = () => {
+    editor.addRectangle();
+  };
+
   return (
-    <div>
-      <h1> APP here</h1>
+    <div className="App">
+      <h1>FabricJS React Sample</h1>
+      <button onClick={onAddCircle}>Add circle</button>
+      <button onClick={onAddRectangle}>Add Rectangle</button>
+      <FabricJSCanvas className="sample-canvas" onReady={onReady} />
     </div>
   );
 }
 
-export default App;
+// import theShirt from "./img/background_tshirt.png";
+//<img src={logo} alt="Logo" />
