a9cc59871af705c669c7ab834afcddf778a5dcc3
Igor Risteski <pirej30@yahoo.com>
2020-12-04 08:34:22 +0100

    Move all base-shirt related code in to a separate component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import "./App.css";
-import theShirt from "./img/background_tshirt.png";
-// import batman from "./img/batman2.png";
-import React, { useState } from "react";
-import ColorPicker from "./components/ColorPicker";
+import React from "react";
+import BaseShirt from "./components/BaseShirt";
 import { FabricJSCanvas, useFabricJSEditor } from "fabricjs-react";
 
 export default function App() {
@@ -14,38 +12,14 @@ export default function App() {
     editor.addRectangle();
   };
 
-  const [shirtColor, setShirtColor] = useState("");
-
-  console.log(shirtColor);
-
-  function changeColor(event) {
-    event.preventDefault();
-    setShirtColor("green");
-  }
-  console.log(shirtColor);
-
   return (
     <div className="App">
       <h1>Simple t-shirt designer with FabricJS and ReactJS</h1>
       <button onClick={onAddCircle}>Add circle</button>
       <button onClick={onAddRectangle}>Add Rectangle</button>
       <FabricJSCanvas className="sample-canvas" onReady={onReady} />
-      <img
-        style={{ backgroundColor: shirtColor }}
-        src={theShirt}
-        alt="theShirt"
-        className="shirt"
-      />
 
-      <button onClick={changeColor}>Color</button>
-
-      <ColorPicker />
+      <BaseShirt />
     </div>
   );
 }
-
-// value="#fff">White
-// value="#000">Black
-// value="#f00">Red
-// value="#008000">Green
-// value="#ff0">Yellow
---
+++ src/components/BaseShirt.js
@@ -0,0 +1,93 @@
+import React, { useState } from "react";
+import "./BaseShirt.css";
+import theShirt from "../img/background_tshirt.png";
+
+function BaseShirt() {
+  const [shirtColor, setShirtColor] = useState("");
+
+  function changeColor(event) {
+    event.preventDefault();
+    setShirtColor(event.target.className);
+    console.log(event);
+  }
+
+  return (
+    <div>
+      <h2>Change shirt color</h2>
+      <button
+        className="violet"
+        id="btn-style"
+        style={{ backgroundColor: "violet", color: "black" }}
+        onClick={changeColor}
+      ></button>
+      <button
+        className="crimson"
+        id="btn-style"
+        style={{ backgroundColor: "crimson", color: "white" }}
+        onClick={changeColor}
+      ></button>
+      <button
+        className="orange"
+        id="btn-style"
+        style={{ backgroundColor: "orange", color: "black" }}
+        onClick={changeColor}
+      ></button>
+      <button
+        className="gold"
+        id="btn-style"
+        style={{ backgroundColor: "gold", color: "black" }}
+        onClick={changeColor}
+      ></button>
+      <button
+        className="seagreen"
+        id="btn-style"
+        style={{ backgroundColor: "seagreen", color: "white" }}
+        onClick={changeColor}
+      ></button>
+      <button
+        className="royalblue"
+        id="btn-style"
+        style={{ backgroundColor: "royalblue", color: "white" }}
+        onClick={changeColor}
+      ></button>
+      <button
+        className="navy"
+        id="btn-style"
+        style={{ backgroundColor: "navy", color: "white" }}
+        onClick={changeColor}
+      ></button>
+      <button
+        className="black"
+        id="btn-style"
+        style={{ backgroundColor: "black", color: "white" }}
+        onClick={changeColor}
+      ></button>
+      <button
+        className="darkslategrey"
+        id="btn-style"
+        style={{ backgroundColor: "darkslategrey", color: "white" }}
+        onClick={changeColor}
+      ></button>
+      <button
+        className="slategrey"
+        id="btn-style"
+        style={{ backgroundColor: "slategrey", color: "white" }}
+        onClick={changeColor}
+      ></button>
+      <button
+        className="whitesmoke"
+        id="btn-style"
+        style={{ backgroundColor: "whitesmoke", color: "black" }}
+        onClick={changeColor}
+      ></button>
+      <img
+        style={{ backgroundColor: shirtColor }}
+        src={theShirt}
+        alt="theShirt"
+        className="shirt"
+      />
+    </div>
+  );
+}
+
+export default BaseShirt;
