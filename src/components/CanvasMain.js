237e9b3da0d89fcf100d53375b0f3bca0e7758ad
Igor Risteski <pirej30@yahoo.com>
2020-12-08 00:04:13 +0100

    Blind copy the FabricJS full app But.. not happy with it

--- src/components/CanvasMain.js
+++ src/components/CanvasMain.js
@@ -1,33 +1,88 @@
 import "./CanvasMain.css";
-import React, {useContext} from "react";
+import React, {useContext, useState} from "react";
 // import BaseShirt from "./components/BaseShirt";
 import { ColorContext } from "./colorContext";
 import { FabricJSCanvas, useFabricJSEditor } from "fabricjs-react";
 
 function CanvasMain(props) {
   const { updateColor } = useContext(ColorContext);
-  const { editor, onReady } = useFabricJSEditor();
-  const onAddCircle = () => {
-    editor.addCircle();
-  };
-  const onAddRectangle = () => {
-    editor.addRectangle();
-  };
-  const onAddLine = () => {
-    editor.addLine();
-  };
+  const { selectedObjects, editor, onReady } = useFabricJSEditor({ defaultStrokeColor: 'red' })
+  const [text, setText] = useState("");
+  const [strokeColor, setStrokeColor] = useState("");
+  const [fillColor, setFillColor] = useState("");
 
+  const onAddCircle = () => {
+    editor?.addCircle()
+  }
+  const onAddRectangle = () => {
+    editor?.addRectangle()
+  }
+  const onAddLine = () => {
+    editor?.addLine()
+  }
+  const onAddText = () => {
+    if (selectedObjects?.length) {
+      return editor?.updateText(text)
+    }
+    editor?.addText(text);
+  }
+  const onSetStrokeColor = () => {
+    editor?.setStrokeColor(strokeColor)
+  }
+  const onSetFillColor = () => {
+    editor?.setFillColor(fillColor)
+  }
+  const onDeleteAll = () => {
+    editor?.deleteAll();
+  }
+  const onDeleteSelected = () => {
+    editor?.deleteSelected();
+  }
+  const onZoomIn = () => {
+    editor?.zoomIn()
+  }
+  const onZoomOut = () => {
+    editor?.zoomOut()
+  }
   return (
     <div className="mainCanvasDiv">
       <h2>Design here</h2>
-      <button onClick={onAddCircle}>Add circle</button>
-      <button onClick={onAddRectangle}>Add Rectangle</button>
-      <button onClick={onAddLine}>Add Line</button>
+      {editor ? (<div>
+        <button onClick={onZoomIn}>Zoom In</button>
+        <button onClick={onZoomOut}>Zoom Out</button>
+        <button onClick={onAddCircle}>Add circle</button>
+        <button onClick={onAddRectangle}>Add Rectangle</button>
+        <button onClick={onAddLine}>Add Line</button>
+        <button onClick={onDeleteAll}>Delete All</button>
+        <button onClick={onDeleteSelected}>Delete Selected</button>
+        <input
+          type="text"
+          value={text}
+          onChange={e => setText(e.target.value)}
+        />
+        <button onClick={onAddText}>Add Text</button>
+        <input
+          type="text"
+          value={strokeColor || editor.strokeColor}
+          onChange={e => setStrokeColor(e.target.value)}
+        />
+        <button onClick={onSetStrokeColor}>Set Stroke Color</button>
+        <input
+          type="text"
+          value={fillColor || editor.fillColor}
+          onChange={e => setFillColor(e.target.value)}
+        />
+        <button onClick={onSetFillColor}>Set Fill Color</button>
+
+        <pre>fillColor: {editor.fillColor}, strokeColor: {editor.strokeColor}</pre>
+        <pre>{JSON.stringify(selectedObjects)}</pre>
+
+      </div>) : (<>Loading...</>)}
       <div className="bigCanvas" style={{backgroundColor: updateColor}}>
-      <FabricJSCanvas className="main-canvas" onReady={onReady} />
+        <FabricJSCanvas className="main-canvas" onReady={onReady} />
       </div>
     </div>
-  );
+  )
 }
 
-export default CanvasMain;
+export default CanvasMain
