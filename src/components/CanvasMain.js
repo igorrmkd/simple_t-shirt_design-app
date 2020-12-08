0122d965c8909585bd67f15640ba0563e65e9373
Igor Risteski <pirej30@yahoo.com>
2020-12-08 15:48:11 +0100

    Try some free-hand drawing

--- src/components/CanvasMain.js
+++ src/components/CanvasMain.js
@@ -1,30 +1,85 @@
 import "./CanvasMain.css";
-import React, {useContext} from "react";
-// import BaseShirt from "./components/BaseShirt";
+import React, {useContext, useRef, useEffect, useState} from "react";
 import { ColorContext } from "../contexts/colorContext";
-import { FabricJSCanvas, useFabricJSEditor } from "fabricjs-react";
 
 function CanvasMain(props) {
   const { updateColor } = useContext(ColorContext);
-  const { editor, onReady } = useFabricJSEditor();
-  const onAddCircle = () => {
-    editor.addCircle();
-  };
-  const onAddRectangle = () => {
-    editor.addRectangle();
-  };
-  const onAddLine = () => {
-    editor.addLine();
-  };
+//  Drawing freehand 
 
+  const canvasRef = useRef(null)
+  const contextRef = useRef(null)
+  const [isDrawing, setIsDrawing] = useState(false)
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    canvas.width = `606` * 2;
+    canvas.height = `686` * 2;
+    canvas.style.width = `606px`;
+    canvas.style.height = `686px`;
+
+
+    const context = canvas.getContext("2d")
+    context.scale(2,2)
+    context.lineCap = "round"
+    context.strokeStyle = "black"
+    context.lineWidth = 5
+    contextRef.current = context;
+  }, [])
+
+  const startDrawing = ({nativeEvent}) => {
+    const {offsetX, offsetY} = nativeEvent;
+    contextRef.current.beginPath()
+    contextRef.current.moveTo(offsetX, offsetY)
+    setIsDrawing(true)
+  }
+
+  const finishDrawing = () => {
+   contextRef.current.closePath()
+   setIsDrawing(false)
+  }
+
+  const draw = ({nativeEvent}) => {
+    if(!isDrawing){
+      return
+    }
+    const {offsetX, offsetY} = nativeEvent;
+    contextRef.current.lineTo(offsetX, offsetY)
+    contextRef.current.stroke()
+  }
+
+  // end drawing freehand
+
+  // clear the canvas
+ 
+  function clearCanvas() {
+      const canvas = canvasRef.current;
+      const context = canvas.getContext("2d")
+      // context.fillStyle = {backgroundColor: updateColor}
+      context.fillStyle = updateColor
+      context.fillRect(0, 0, canvas.width, canvas.height)
+  }
+
+  // end clear the canvas
+    
   return (
     <div className="mainCanvasDiv">
       <h2>Design here</h2>
-      <button onClick={onAddCircle}>Add circle</button>
-      <button onClick={onAddRectangle}>Add Rectangle</button>
-      <button onClick={onAddLine}>Add Line</button>
+
+
+      <div className="clearButton">
+        <button ref={canvasRef} onClick={clearCanvas}>Clear</button>
+      </div>
+
+
+
       <div className="bigCanvas" style={{backgroundColor: updateColor}}>
-      <FabricJSCanvas className="main-canvas" onReady={onReady} />
+      <canvas 
+      className="main-canvas"
+      onMouseDown={startDrawing}
+      onMouseUp={finishDrawing}
+      onMouseMove={draw}
+      ref={canvasRef}
+       />
       </div>
     </div>
   );
